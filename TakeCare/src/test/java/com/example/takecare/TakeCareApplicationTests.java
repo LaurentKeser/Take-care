1442c0181232ff565247d5e5a2b23d084bb343bc
52167 <52167@etu.he2b.be>
2022-01-26 14:15:43 +0100

    initial commit

---
+++ TakeCare/src/test/java/com/example/takecare/TakeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
